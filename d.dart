6047e83fbcf88b116b2ed758d750ddb852a58d7d
d1n2o3b4 <davebostons2017@gmail.com>
2023-08-14 20:57:15 +0100

    performed string concat

--- d.dart
+++ d.dart
@@ -5,9 +5,10 @@ void main() {
   dynamic diddy;
   dynamic soop = "Peter";
   dynamic name = 12;
-  //Over here even though i assigned values it doesn't mean they are restricted
-  //to that datatype
-
+  /*Over here even though i assigned values it doesn't mean they are restricted
+  to that datatype*/
   print("WAGUAN");
-  print(soop);
+  /*To do string concat with vars you need to include the var in the string but
+	have a $ infront of it.*/
+  print("I am called $soop and i am $age years old");
 }
