dbd7b050596a1b17cfe96f2000733f04af8cf412
d1n2o3b4 <davebostons2017@gmail.com>
2023-08-14 20:48:34 +0100

    added variables and keywords

--- d.dart
+++ d.dart
@@ -1,3 +1,13 @@
 void main() {
+  final int var1 = 34;
+  const int age = 23;
+  //Dynamic is used when we don't want to specify the variable type yet
+  dynamic diddy;
+  dynamic soop = "Peter";
+  dynamic name = 12;
+  //Over here even though i assigned values it doesn't mean they are restricted
+  //to that datatype
+
   print("WAGUAN");
+  print(soop);
 }
