2e040f4eb4d631fc1058ce9f8dfe48652e6847c0
d1n2o3b4 <davebostons2017@gmail.com>
2023-08-16 14:04:44 +0100

    used the tilde division

--- hello_dart_proj/bin/hello_dart_proj.dart
+++ hello_dart_proj/bin/hello_dart_proj.dart
@@ -3,4 +3,5 @@ import 'package:hello_dart_proj/hello_dart_proj.dart' as hello_dart_proj;
 //we did not need the List arg
 void main() {
   print('Hello world: ${hello_dart_proj.calculate()}!');
+  print(23 ~/ 3);
 }
