3c3e931a060e1815eab406a6008f29a0eb1c4be5
d1n2o3b4 <davebostons2017@gmail.com>
2023-08-19 20:59:28 +0100

    created a constructor for the car class

--- c.dart
+++ c.dart
@@ -5,4 +5,12 @@ void main() {
 class Car {
   String? brand, version, color;
   int? year;
+
+  //Constructor for car class
+  Car(String c_brand, c_version, c_color, c_year) {
+    this.brand = c_brand;
+    this.version = c_version;
+    this.color = c_color;
+    this.year = c_year;
+  }
 }
