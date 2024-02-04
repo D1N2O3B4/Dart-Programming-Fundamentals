9b1d50926a4c695f96cb60beec503a9af4699eba
d1n2o3b4 <davebostons2017@gmail.com>
2024-02-04 21:30:25 +0000

    added a final variable

--- genesis/bin/genesis.dart
+++ genesis/bin/genesis.dart
@@ -2,6 +2,8 @@ import 'package:genesis/genesis.dart' as genesis;
 import 'dart:math';
 
 void main(List<String> arguments) {
+  //var/const/final
+  final date = DateTime(2012);
   print('Hello world: ${genesis.calculate()}!');
   print(sin(90));
   double v = 1 / sqrt(2);
