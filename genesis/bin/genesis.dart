f8ffc6e1f94c313a756d51ff73e8f77720f8a760
d1n2o3b4 <davebostons2017@gmail.com>
2024-02-25 20:08:52 +0000

    added tenary ops

--- genesis/bin/genesis.dart
+++ genesis/bin/genesis.dart
@@ -8,11 +8,16 @@ void main(List<String> arguments) {
   print(sin(90));
   double v = 1 / sqrt(2);
   print("And value is ${sin(v)}");
-  //optional values
+  //optional values cannot have null final variable
   String? name;
   name = null;
   print(name?.length);
   print(date);
+  //For this code the ?? is a condition so if null make it 0 and not null
+  print(name?.length ?? 0);
+  //Tenary operator
+  int age = 23;
+  print(age > 20 ? "Big boy $age" : "No you are too young");
 
   ///docs commentation in flutter
 }
